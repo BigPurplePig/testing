833ac058fcccc86ae8b69a9a7bc5ab63257e2a7f
fori <124411319+fori404@users.noreply.github.com>
2025-12-02 14:16:01 -0500

    hover size change

--- button.gd
+++ button.gd
@@ -1,5 +1,6 @@
-extends Label
+extends Button
 
+const SIZE_DIFF = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +12,10 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	randomize()
-	var roll = randi_range(1, 6)
-	text = str(roll)
+func _on_mouse_entered() -> void:
+	scale = Vector2(1 + SIZE_DIFF, 1 + SIZE_DIFF)
+	position -= size * SIZE_DIFF * 0.5
+
+func _on_mouse_exited():
+	scale = Vector2.ONE
+	position += size * SIZE_DIFF * 0.5
