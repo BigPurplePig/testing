83cc1c4b61cb3dd930f5274fe495371bce6260d9
fori <124411319+fori404@users.noreply.github.com>
2025-12-05 16:52:59 -0500

    added cooler buttons

--- button.gd
+++ button.gd
@@ -2,20 +2,36 @@ extends Button
 
 const SIZE_DIFF = 0.05
 
-# Called when the node enters the scene tree for the first time.
+var origin = position
+var offset = position - size * SIZE_DIFF * 0.5
+var offset_less = position - size * SIZE_DIFF * 0.25
+
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_mouse_entered() -> void:
-	scale = Vector2(1 + SIZE_DIFF, 1 + SIZE_DIFF)
-	position -= size * SIZE_DIFF * 0.5
+	var tween = create_tween()
+	
+	tween.tween_property(self, "scale", Vector2(1 + SIZE_DIFF, 1 + SIZE_DIFF), 0.1).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(self, "position", offset, 0.1).set_trans(Tween.TRANS_SINE)
 
-func _on_mouse_exited():
-	scale = Vector2.ONE
-	position += size * SIZE_DIFF * 0.5
+func _on_mouse_exited() -> void:
+	var tween = create_tween()
+	
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.1).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(self, "position", origin, 0.1).set_trans(Tween.TRANS_SINE)
+
+func _on_button_down() -> void:
+	var tween = create_tween()
+	
+	tween.tween_property(self, "scale", Vector2(1 + SIZE_DIFF * 0.5, 1 + SIZE_DIFF * 0.5), 0.1).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(self, "position", offset_less, 0.1).set_trans(Tween.TRANS_SINE)
+
+func _on_button_up() -> void:
+	var tween = create_tween()
+	
+	tween.tween_property(self, "scale", Vector2(1 + SIZE_DIFF, 1 + SIZE_DIFF), 0.1).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(self, "position", offset, 0.1).set_trans(Tween.TRANS_SINE)
